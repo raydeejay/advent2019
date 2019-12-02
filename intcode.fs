95b812e73b94d0b265bc8242f9e8333cd032fc2d
Sergi Reyner <sergi.reyner@gmail.com>
2019-12-02 16:24:55 +0000

    Separated `pointer` from `ip` and reorganized the code a bit

--- intcode.fs
+++ intcode.fs
@@ -7,22 +7,9 @@
 : digit?  ( c -- f )  [CHAR] 0 [CHAR] 9 1+ WITHIN ;
 
 
-\ MEMORY -----------------------------------------------
-4096 CONSTANT memory-size
-memory-size array bytecode
-memory-size array bytecode-backup
-
-: save-bytecode  ( -- )  bytecode bytecode-backup memory-size MOVE ;
-: restore-bytecode  ( -- )  bytecode-backup bytecode memory-size MOVE ;
-
-bytecode CONSTANT output
-bytecode CELL+ CONSTANT noun
-bytecode 2 CELLS + CONSTANT verb
-
-
 \ I/O --------------------------------------------------
 VARIABLE input-file
-VARIABLE pointer
+VARIABLE bytecode-pointer
 VARIABLE input-char
 
 : open-input  ( filename -- )
@@ -32,13 +19,29 @@ VARIABLE input-char
 : read-digit  ( -- )
   input-char 1 input-file @ READ-FILE ABORT" Error reading digit" ;
 : load-digit  ( -- )
-  pointer @ @ 10 *              \ get current value * 10
+  bytecode-pointer @ @ 10 *     \ get current value * 10
   input-char @ [CHAR] 0 -  +    \ add the input digit
-  pointer @ ! ;                 \ store it back
-: skip-non-digit  ( -- )  1 CELLS pointer +! ;
+  bytecode-pointer @ ! ;        \ store it back
+: skip-non-digit  ( -- )  1 CELLS bytecode-pointer +! ;
 
+
+\ BYTECODE ---------------------------------------------
+4096 CONSTANT memory-size
+
+memory-size array bytecode
+memory-size array bytecode-backup
+
+bytecode CONSTANT output
+bytecode CELL+ CONSTANT noun
+bytecode 2 CELLS + CONSTANT verb
+
+VARIABLE ip
+
+: save-bytecode  ( -- )  bytecode bytecode-backup memory-size MOVE ;
+: restore-bytecode  ( -- )  bytecode-backup bytecode memory-size MOVE ;
 : clear-bytecode  ( -- ) bytecode memory-size CELLS ERASE ;
 : read-bytecode  ( -- )
+  bytecode bytecode-pointer !
   BEGIN read-digit
   WHILE
     input-char @ digit? IF
@@ -48,28 +51,26 @@ VARIABLE input-char
   REPEAT ;
 : load-bytecode  ( filename -- )
   clear-bytecode
-  bytecode pointer !
   open-input
   read-bytecode
-  close-input
-  bytecode pointer ! ;
+  close-input ;
 
 
 \ OPCODES ----------------------------------------------
 : fetch-arguments  ( -- addr u1 u2 )
-  pointer @ CELL+ @             \ first bytecode offset argument
+  ip @ CELL+ @             \ first bytecode offset argument
   CELLS bytecode + @            \ dereference
-  pointer @ 2 CELLS + @         \ second bytecode offset
+  ip @ 2 CELLS + @         \ second bytecode offset
   CELLS bytecode + @            \ dereference
-  pointer @ 3 CELLS + @  CELLS bytecode + \ destination *address*
+  ip @ 3 CELLS + @  CELLS bytecode + \ destination *address*
   -ROT ;
 
 : op-add      ( -- )       fetch-arguments + SWAP ! ;
 : op-mult     ( -- )       fetch-arguments * SWAP ! ;
 : op-halt     ( f -- f' )  DROP 0 ;
 : op-illegal  ( f -- f' )
-  ." Illegal opcode #" HEX pointer @ @ DECIMAL
-  ."  at position #" HEX pointer @ bytecode - . DECIMAL CR
+  ." Illegal opcode #" HEX ip @ @ DECIMAL
+  ."  at position #" HEX ip @ bytecode - . DECIMAL CR
   op-halt ;
 
 2 CELLS CONSTANT /opcode         \ bytes in table for each key
@@ -92,9 +93,9 @@ HERE /opcode - CONSTANT 'nomatch \ addr of no-match function
 
 
 \ EXECUTION --------------------------------------------
-: cycle  ( -- f )  -1  pointer @ @ 'function CELL+ @ EXECUTE ;
-: advance  ( -- )  4 CELLS pointer +! ;
+: cycle  ( -- f )  -1  ip @ @ 'function CELL+ @ EXECUTE ;
+: advance  ( -- )  4 CELLS ip +! ;
 : patch-program  ( noun verb -- )  verb ! noun ! ;
 : execute-program  ( -- )
-  bytecode pointer !
+  bytecode ip !
   BEGIN cycle WHILE advance REPEAT ;
