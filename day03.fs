fdc8df887e6997ed5ee994c501006cc3d4d45a43
Sergi Reyner <sergi.reyner@gmail.com>
2019-12-04 10:26:46 +0000

    Refactored day 3 a bit

--- day03.fs
+++ day03.fs
@@ -1,8 +1,9 @@
-\ https://adventofcode.com/2019/day/3
+\ https://adventofcode.com/2019/day/3#part2
 
 \ UTILITIES ----------------------------------------
 : UNDER+  ( a b c -- a+c b )  ROT + SWAP ;
 : UNDER-  ( a b c -- a+c b )  ROT SWAP - SWAP ;
+: array CREATE ALLOT ;
 
 
 \ I/O ----------------------------------------------
@@ -12,13 +13,12 @@ VARIABLE input-file
   s" input03.txt" R/O OPEN-FILE ABORT" error opening file" input-file ! ;
 : close-input  ( -- ) input-file @ CLOSE-FILE ABORT" error closing file" ;
 
-: array CREATE ALLOT ;
 4096 CONSTANT line-length
 line-length array str
 
 : read-wire-data  ( -- n f )
   str line-length ERASE
-  str line-length input-file @ READ-LINE ABORT" error" ;
+  str line-length input-file @ READ-LINE ABORT" error" DROP ;
 
 
 \ DIRECTIONS----------------------------------------
@@ -35,10 +35,20 @@ line-length array str
     ENDCASE
   UNTIL
   ptr ;
-: parse-number  ( addr n -- u )  2>R 0 0 2R> >NUMBER DROP ;
+: parse-number  ( addr n -- u addr' )  2>R 0 0 2R> >NUMBER DROP NIP ;
 
 
 \ SEGMENTS -----------------------------------------
+4 CONSTANT /segment
+
+: ox  ( segment -- n )  ;
+: oy  ( segment -- n )  CELL+ ;
+: ux  ( segment -- n )  2 CELLS + ;
+: uy  ( segment -- n )  3 CELLS + ;
+
+: vertical?  ( segment -- f ) DUP ox @ SWAP ux @ = ;
+: horizontal?  ( segment -- f ) DUP oy @ SWAP uy @ = ;
+
 : make-segment  ( ox oy dir length -- ox oy ux uy )
   SWAP CASE
     [CHAR] U OF >R 2DUP R> + ENDOF
@@ -47,53 +57,56 @@ line-length array str
     [CHAR] L OF >R 2DUP R> UNDER- ENDOF
     ." Invalid direction!" CR ABORT
   ENDCASE ;
-: normalize-segment  ( ox oy ux uy -- ox' oy' ux' uy' )
-  { ox oy ux uy | spare -- ox' oy' ux' uy' }
-  ux ox < IF  ox TO spare  ux TO ox  spare TO ux  THEN
-  uy oy < IF  oy TO spare  uy TO oy  spare TO uy  THEN
-  ox oy ux uy ;
 
-: vertical?  ( ox oy ux uy -- f )  DROP NIP = ;
-: horizontal?  ( ox oy ux uy -- f ) NIP ROT DROP = ;
+: normalize-segment  ( segment -- ox oy ux uy )
+  { s }
+  s ox @ s ux @ 2DUP > IF SWAP THEN
+  s oy @ s uy @ 2DUP > IF SWAP THEN
+  ROT SWAP ;
+
+: store-segment  ( ox oy ux uy dest -- )
+  { dest }
+  dest uy !  dest ux !  dest oy !  dest ox ! ;
+: fetch-segment  ( addr -- ox oy ux uy )
+  { addr }
+  addr ox @  addr oy @  addr ux @  addr uy @ ;
+
+: print-segment  ( addr -- )
+  { addr }
+  /segment 0 DO addr I CELLS + ? LOOP CR ;
+
+: intersects-vertically?  ( sv sh -- f )
+  { sv sh -- f }
+  sv ox @  sh ox @ sh ux @ 1+ WITHIN
+  sh oy @  sv oy @ sv uy @ 1+ WITHIN
+  AND ;
+: intersects-horizontally?  ( sv sh -- f )
+  { sv sh -- f }
+  sv ox @  sh ox @ sh ux @ 1+ WITHIN
+  sh oy @  sv oy @ sv uy @ 1+ WITHIN
+  AND ;
 
 \ check for intersection of two normalized segments
 \ ASSUMPTION: always vertical against horizontal lines
-: intersect?  ( s1* s2* -- coords f | f )
-  { ox1 oy1 ux1 uy1 ox2 oy2 ux2 uy2 -- }
-  ox1 oy1 ux1 uy1 horizontal? IF
-    ox2  ox1 ux1 1+ WITHIN
-    oy1  oy2 uy2 1+ WITHIN
-    AND
-    IF ox2 oy1 -1 ELSE 0 THEN
+: intersect?  ( s1 s2 -- coords f | f )
+  { s1 s2 }
+  s1 horizontal? IF
+    s2 s1 intersects-vertically? IF s2 ox @ s1 oy @ -1 ELSE 0 THEN
   ELSE
-    ox1  ox2 ux2 1+ WITHIN
-    oy2  oy1 uy1 1+ WITHIN
-    AND
-    IF ox1 oy2 -1 ELSE 0 THEN
+    s1 s2 intersects-horizontally? IF s1 ox @ s2 oy @ -1 ELSE 0 THEN
   THEN ;
 
 
 \ SEGMENT ARRAYS (WIRES) ---------------------------
-2000 CELLS 4 * ALLOCATE ABORT" Error allocating memory" CONSTANT wire1
-2000 CELLS 4 * ALLOCATE ABORT" Error allocating memory" CONSTANT wire2
+: segments  ( n -- n' )  /segment * CELLS ;
+: segment+  ( addr -- addr' )  1 segments + ;
+: th-segment  ( addr i -- addr' )  /segment * CELLS + ;
+
+2000 segments ALLOCATE ABORT" Error allocating memory" CONSTANT wire1
+2000 segments ALLOCATE ABORT" Error allocating memory" CONSTANT wire2
 
 VARIABLE #wire1
 VARIABLE #wire2
-
-: store-segment  ( ox oy ux uy dest -- )
-  { dest }
-  dest 3 CELLS + !
-  dest 2 CELLS + !
-  dest CELL+ !
-  dest ! ;
-: fetch-segment  ( addr -- ox oy ux uy )
-  { addr }
-  addr @
-  addr CELL+ @
-  addr 2 CELLS + @
-  addr 3 CELLS + @ ;
-
-: print-segment  ( addr -- ) { addr } 4 0 DO addr I CELLS + ? LOOP CR ;
 
 : build-wire-array  ( dest addr n -- )
   { dest ptr len | max x y dir count }
@@ -105,14 +118,15 @@ VARIABLE #wire2
     ptr get-next-direction TO ptr
     ptr C@ TO dir
     ptr 1+ TO ptr
-    ptr 8 parse-number TO ptr DROP TO count
+    ptr 8 parse-number TO ptr TO count
     \ add a new segment to the array
     x y dir count make-segment
     2DUP TO y TO x
     dest store-segment
-    dest 4 CELLS + TO dest
+    dest segment+ TO dest
   REPEAT
   ." Stored segments" CR ;
+
 
 \ MANHATTAN DISTANCE -------------------------------
 VARIABLE best-x
@@ -126,8 +140,8 @@ VARIABLE best-mhd
   { | x y d }
   #wire1 @ 0 DO
     #wire2 @ 0 DO
-      wire1 J CELLS 4 * + fetch-segment
-      wire2 I CELLS 4 * + fetch-segment
+      wire1 J th-segment
+      wire2 I th-segment
       intersect? IF
         TO y TO x
         x y mhd  0>  x y mhd best-mhd @ <  AND IF
@@ -138,23 +152,24 @@ VARIABLE best-mhd
     LOOP
   LOOP ;
 
+: normalize-wire  ( addr count -- )
+  { addr count }
+  count 0 DO
+    addr I th-segment
+    normalize-segment
+    addr I th-segment store-segment
+  LOOP
+;
+
+
 \ ENTRY POINT --------------------------------------
 : go  ( -- )
   open-input
-  read-wire-data DROP #wire1 !
+  read-wire-data #wire1 !
   wire1 str #wire1 @ build-wire-array
-  read-wire-data DROP #wire2 !
+  read-wire-data #wire2 !
   wire2 str #wire2 @ build-wire-array
   close-input
-  \ normalize segments
-  #wire1 @ 0 DO
-    wire1 I CELLS 4 * + fetch-segment
-    normalize-segment
-    wire1 I CELLS 4 * + store-segment
-  LOOP
-  #wire2 @ 0 DO
-    wire2 I CELLS 4 * + fetch-segment
-    normalize-segment
-    wire2 I CELLS 4 * + store-segment
-  LOOP
+  wire1 #wire1 @ normalize-wire
+  wire2 #wire2 @ normalize-wire
   get-nearest-crossing ;
